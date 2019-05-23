052eb7a8020f0a098c477125b5b8cede75a773a4
ishaqsai <50690325+ishaqsai@users.noreply.github.com>
2019-05-23 13:18:57 -0400

    secondcommit

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/parttime.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author saima ishaq
+ */
+public class parttime {
+    public static void mian(String[] args)
+    {
+       System.out.println("hi");
+    }
+    
+}
